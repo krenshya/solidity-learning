e091c5b4cafc41a1469e54198bde8819fe9957e0
krenshya <53909569+krenshya@users.noreply.github.com>
2026-05-24 02:55:36 +0200

    Add Budapest Builders NFT (ERC721)
    Refactored BudapestBuilders contract to update imports and constructor.

---
+++ contracts/BudapestBuilders.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.6.0
+pragma solidity ^0.8.27;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract BudapestBuilders is ERC721, ERC721Enumerable, Ownable {
+    uint256 private _nextTokenId;
+    ...
+}
